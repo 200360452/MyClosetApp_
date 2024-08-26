c77a46c251edd4977a4b5a40797f829cd5a97ab1
Valerie Dupuy <200360452@edu.clg.qc.ca>
2024-08-26 14:57:27 -0400

    modified metro.config.mjs

---
+++ metro.config.mjs
@@ -0,0 +1,11 @@
+// metro.config.mjs
+export default {
+  transformer: {
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: false,
+      },
+    }),
+  },
+};
